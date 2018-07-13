eb01adffc1ea30e581b1c679da8aef219a9b3ef2
Aurélien Loyer <aur.loy@gmail.com>
2018-07-13 16:34:24 +0200

    feat: move project to nuxt 🦄

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    head: {
+      title: 'Talks | Loyer Aurélien',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', content: "Aurélien Loyer - Talks list and links to slides, contents, movies. " }
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Dosis' },
+        { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.1.0/css/all.css', integrity: 'sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt', crossorigin: 'anonymous'},
+        { rel: 'stylesheet', href: 'main.css' }
+      ]
+    }
+  }
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <h1>Talks</h1>
+
+        <section class="bricks">
+
+            <v-talk></v-talk>
+
+        </section>
+    </section>
+</template>
+
+<script>
+
+    import talks from '../talks.json'
+    import Talk from '../components/Talk'
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        components: {
+            'v-talk': Talk,
+        },
+        created() {
+            console.log(talks);
+        },
+    }
+</script>
+
+<style scoped>
+</style>
